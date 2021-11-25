e4f85173e8770afff3b25d89a3d12d4e271b083e
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-25 19:53:49 +0300

    Add fake data for the slider

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const sliderItems = [
+  {
+    id: 1,
+    smallTitle: 'Best Furniture For Your Castle....',
+    mainTitle: 'New Furniture Collection Trends in 2021',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.',
+    img: 'https://i.imgur.com/qW0jsNY.png',
+    bgColor: '#ecd2fa35'
+  },
+  {
+    id: 2,
+    smallTitle: 'Best Furniture For Your Castle....',
+    mainTitle: 'New Trends In This Summer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.',
+    img: 'https://i.imgur.com/G4sBVT4.png',
+    bgColor: '#b9d4ff35'
+  },
+  {
+    id: 3,
+    smallTitle: 'Best Furniture For Your Castle....',
+    mainTitle: 'Free Shipping On All Orders Over $100',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.',
+    img: 'https://i.imgur.com/wE2Pn3O.png',
+    bgColor: '#f0ffb935'
+  }
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1589584649628-b597067e07a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    title: 'CHAIR'
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1595268005256-f9032d8d49fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    title: 'ELECTRONIC'
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1475180429745-7bdddbdf4e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+    title: 'BACKPACK'
+  }
+]
