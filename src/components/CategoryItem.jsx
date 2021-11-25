b3529526098d0171b9cb153f53f04e9cc5967eaa
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-11-25 22:40:48 +0300

    Create CategoryItem component

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    position: relative;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+`
+const InfoSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`
+const Title = styled.h3`
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 32px;
+    color: white;
+    text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
+    z-index: 4;
+`
+const Button = styled.button`
+    outline: none;
+    border: 1.5px solid white;
+    padding: 20px;
+    font-size: 16px;
+    font-weight: 700;
+    background: #ffffff10;
+    color: white;
+    cursor: pointer;
+
+    &:hover {
+        background: white;
+        color: black;
+    }
+`
+
+const CategoryItem = ({ category }) => {
+    return (
+        <Container>
+            <Image src={category.img} />
+            <InfoSection>
+                <Title>{category.title}</Title>
+                <Button>SHOP NOW</Button>
+            </InfoSection>
+        </Container>
+    )
+}
+
+export default CategoryItem
