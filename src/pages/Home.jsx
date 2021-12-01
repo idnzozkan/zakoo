f7d2b9e4c22c9c638d727b1fab9d506cb23af974
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-01 16:37:09 +0300

    Remove newsletter import on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
 import Products from '../components/Products'
-import Newsletter from '../components/Newsletter'
 import MainLayout from '../layouts/MainLayout'
 
 const Home = () => {
