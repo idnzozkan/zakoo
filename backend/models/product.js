b7f79ecd1bc4115a98d4cbd5fe24ae49d67e920b
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-02 16:16:09 +0300

    Create db models

---
+++ backend/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    image: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    categories: {
+      type: Array
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    color: {
+      type: String
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
