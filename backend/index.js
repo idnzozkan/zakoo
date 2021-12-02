ccbb95d0386a16411fca4f02bf497b6bd2d4cb5f
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-02 17:48:33 +0300

    Update route importing names with singular names

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dontenv = require('dotenv')
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+const productRoute = require('./routes/product')
 
 dontenv.config()
 require('./mongo-connection')
@@ -9,8 +10,9 @@ require('./mongo-connection')
 const app = express()
 app.use(express.json())
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running!')
