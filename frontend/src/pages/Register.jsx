bca2a800dbde41dbe2273b369886548a7d401109
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-07 20:51:15 +0300

    Hide login and register pages if user logged in

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 import styled from 'styled-components'
 import LoginRegisterForm from '../components/LoginRegisterForm'
 import MainLayout from '../layouts/MainLayout'
 import { xsmall } from '../responsive'
 
 const Login = () => {
+    let isUserLoggedIn = true
+
+    if (isUserLoggedIn) return <Navigate to="/" />
+
     return (
         <MainLayout>
             <Container>
