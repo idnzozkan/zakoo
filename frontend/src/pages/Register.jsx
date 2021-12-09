aa5e8065f5ec566d015c467f44592be83b06fe9c
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-09 17:25:41 +0300

    Update login and register page structure

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import styled from 'styled-components'
-import LoginRegisterForm from '../components/LoginRegisterForm'
+import RegisterForm from '../components/RegisterForm'
 import MainLayout from '../layouts/MainLayout'
 import { xsmall } from '../responsive'
 
 const Login = () => {
-    let isUserLoggedIn = true
+    const loggedInUser = useSelector(state => state.reducer.user.loggedInUser)
 
-    if (isUserLoggedIn) return <Navigate to="/" />
+    if (loggedInUser) return <Navigate to="/" />
 
     return (
         <MainLayout>
             <Container>
-                <LoginRegisterForm title="Register" description="Please enter the required fields." formType="register" />
+                <Wrapper>
+                    <TextContainer>
+                        <Title>Register</Title>
+                        <Description>Please enter the required fields.</Description>
+                    </TextContainer>
+                    <RegisterForm />
+                </Wrapper>
             </Container>
         </MainLayout>
     )
@@ -23,6 +30,36 @@ const Container = styled.div`
     padding: 100px 370px;
 
    ${xsmall({ padding: '50px 0' })}
+`
+
+const Wrapper = styled.div`
+    margin: auto;
+    padding: 50px 56px;
+    width: 40%;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 25px 10px #F8F8FB;
+
+   ${xsmall({ padding: 0, width: '70%' })}
+`
+
+const TextContainer = styled.div`
+    text-align: center;
+`
+
+const Title = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 32px;
+    line-height: 37.5px;
+    color: #000;
+`
+
+const Description = styled.p`
+    font-size: 17px;
+    line-height: 37.5px;
+    color: #9096B2;
+    margin-bottom: 20px;
+
+   ${xsmall({ lineHeight: 1.75 })}
 `
 
 export default Login
