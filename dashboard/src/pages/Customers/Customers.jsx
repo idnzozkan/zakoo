3388926b5df7e3eedf8bb17104ec83115b9bba2d
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-16 15:56:13 +0300

    Add customers page

---
+++ dashboard/src/pages/Customers/Customers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../layouts/MainLayout/MainLayout'
+
+const Customers = () => {
+    return (
+        <MainLayout>
+            customers
+        </MainLayout>
+    )
+}
+
+export default Customers
