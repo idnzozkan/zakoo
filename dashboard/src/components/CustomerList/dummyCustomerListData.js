8cfa8170469442f1cc27b47bbf845a7a46c1ba61
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-23 19:29:01 +0300

    Add customer list component using dummy data

---
+++ dashboard/src/components/CustomerList/dummyCustomerListData.js
@@ -0,0 +1,94 @@
+const rows = [
+  {
+    id: 0,
+    name: 'Deniz Ozkan',
+    username: 'deniz',
+    email: 'deniz@mail.com',
+    isAdmin: true,
+    avatar: 'https://avatars.githubusercontent.com/u/59365742?v=4',
+    status: 'active'
+  },
+  {
+    id: 1,
+    name: 'Jon Snow',
+    username: 'jon.snow0',
+    email: 'jonsnow@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/men/35.jpg',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Cersei Lannister',
+    username: 'cersei.lannister',
+    email: 'cerseilannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'Jaime Lannister',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/women/36.jpg',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Arya Stark',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/men/36.jpg',
+    status: 'active'
+  },
+  {
+    id: 5,
+    name: 'Targaryen Daenerys',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/women/38.jpg',
+    status: 'active'
+  },
+  {
+    id: 6,
+    name: 'Melisandre',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/men/38.jpg',
+    status: 'active'
+  },
+  {
+    id: 7,
+    name: 'Ferrara Clifford',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/women/39.jpg',
+    status: 'active'
+  },
+  {
+    id: 8,
+    name: 'Rossini Frances',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/men/39.jpg',
+    status: 'active'
+  },
+  {
+    id: 9,
+    name: 'Harvey Roxie',
+    username: 'jaime.lannister',
+    email: 'jaimelannister@mail.com',
+    isAdmin: false,
+    avatar: 'https://randomuser.me/api/portraits/women/40.jpg',
+    status: 'active'
+  }
+]
+
+export default rows
