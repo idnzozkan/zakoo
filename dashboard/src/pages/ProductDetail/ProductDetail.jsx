e2b1cd7ab79e518567cc6775bca9533050c3dfa7
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-30 23:29:04 +0300

    Create product detail page

---
+++ dashboard/src/pages/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../layouts/MainLayout'
+import './product-detail.scss'
+
+const ProductDetail = () => {
+    return (
+        <MainLayout>
+            product detail
+        </MainLayout>
+    )
+}
+
+export default ProductDetail
