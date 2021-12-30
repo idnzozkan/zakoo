bc3699626faa588d4d93b78455195e9c9815550d
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-30 23:35:56 +0300

    Create global state and add user login using redux toolkit

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom'
 import './style/global-styles.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { persistor } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ dashboard/src/store/index.js
@@ -0,0 +1,38 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import userReducer from './user'
+
+const rootReducer = combineReducers({ user: userReducer })
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: {
+    reducer: persistedReducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+export let persistor = persistStore(store)
