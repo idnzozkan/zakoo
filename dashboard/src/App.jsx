9f57d4051fa9f0e39f9a0c01cd8744b773ba0d39
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-30 23:37:58 +0300

    Render login page if an admin is not logged in to the dashboard

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,21 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Orders from './pages/Orders'
-import Products from './pages/Products'
 import Customers from './pages/Customers'
 import CustomerDetail from './pages/CustomerDetail'
+import NewCustomer from './pages/NewCustomer'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import NewProduct from './pages/NewProduct'
+import Login from './pages/Login'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  const token = useSelector(store => store.reducer.user.loggedInUser?.accessToken)
+  const isAdmin = useSelector(store => store.reducer.user.loggedInUser?.isAdmin)
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/customers/:id" element={<CustomerDetail />} />
-      </Routes>
-    </Router>
+    <>
+      {(!token || isAdmin === false) ? <Login /> : (
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/customers/:id" element={<CustomerDetail />} />
+            <Route path="/customers/new" element={<NewCustomer />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/products/new" element={<NewProduct />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   )
 }
 
