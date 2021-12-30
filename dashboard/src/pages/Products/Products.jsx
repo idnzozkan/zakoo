feeb448ca5aba6adcafcb3a03f69b45662f4c0bf
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-30 23:32:03 +0300

    Import required components to Products page

--- dashboard/src/pages/Products/Products.jsx
+++ dashboard/src/pages/Products/Products.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './products.scss'
 import MainLayout from '../../layouts/MainLayout/MainLayout'
+import PageHeader from '../../components/PageHeader/PageHeader'
+import ProductList from '../../components/ProductList/ProductList'
 
 const Products = () => {
     return (
         <MainLayout>
-            products
+            <PageHeader title='Products' btnPath='/products/new' btnType='new' />
+            <ProductList />
         </MainLayout>
     )
 }
