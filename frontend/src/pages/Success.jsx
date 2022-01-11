6e9ce6ba165c4e463a0eefb707fdbe2d4e6bc3c7
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-01-11 23:00:17 +0300

    Create order request on the success page redirection

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { useLocation } from 'react-router'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useLocation, Navigate } from 'react-router'
+import MainLayout from '../layouts/MainLayout'
+import { userRequest } from '../requestMethods'
 
 const Success = () => {
-    const location = useLocation()
-    console.log(location)
+    const data = useLocation().state // specific data object for the current route
+    const id = useSelector(state => state.reducer.user.loggedInUser?._id)
+
+    useEffect(() => {
+        const createOrder = async () => {
+            try {
+                const res = await userRequest.post(`/orders/${id}`, {
+                    products: data?.orderData.products.map(p => ({
+                        productId: p._id,
+                        quantity: p.quantity
+                    })),
+                    amount: data?.orderData.totalPrice,
+                    address: data?.stripeData.billing_details.address,
+                    paymentId: data?.stripeData.id
+                })
+                console.log(res.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        data && createOrder()
+    }, [id, data])
+
+    if (!data) return <Navigate to="/" />
 
     return (
-        <div>
+        <MainLayout>
             Checkout is Successful!
-        </div>
+        </MainLayout>
     )
 }
 
