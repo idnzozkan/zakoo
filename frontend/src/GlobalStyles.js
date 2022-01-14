ab50c244e45474c7b83fc044241ed728fb3275af
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-01-14 21:37:37 +0300

    Use styled components for the global styles

---
+++ frontend/src/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Lato', sans-serif;
+    }
+    
+    a {
+        text-decoration: none;
+    }
+
+`
+
+export default GlobalStyles
