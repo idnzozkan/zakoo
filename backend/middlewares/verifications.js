636935dd3ac8092a76050d0a89ff1727c9fc3421
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-04-12 22:34:19 +0300

    Use early return for the middleware function

--- backend/middlewares/verifications.js
+++ backend/middlewares/verifications.js
@@ -3,18 +3,18 @@ const jwt = require('jsonwebtoken')
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1]
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return res.status(403).json('Invalid token!')
-
-      req.user = user
-      next()
-    })
-  } else {
+  if (!authHeader) {
     return res.status(401).json('You are not authenticated!')
   }
+
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json('Invalid token!')
+
+    req.user = user
+    next()
+  })
 }
 
 const verifyTokenAndAuthorization = (req, res, next) => {
