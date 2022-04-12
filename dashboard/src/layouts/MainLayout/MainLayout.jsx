4ec4ba3b56d7d61f9b74dd0ee4f4fd2647c0a8a9
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-04-12 22:46:13 +0300

    Update layout components

--- dashboard/src/layouts/MainLayout/MainLayout.jsx
+++ dashboard/src/layouts/MainLayout/MainLayout.jsx
@@ -3,6 +3,7 @@ import './main-layout.scss'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Content from './components/Content'
+import { Toaster } from 'react-hot-toast'
 
 const MainLayout = ({ children }) => {
     return (
@@ -11,6 +12,7 @@ const MainLayout = ({ children }) => {
             <div className="container">
                 <Sidebar />
                 <Content>
+                    <Toaster position='top-right' />
                     {children}
                 </Content>
             </div>
