4de131ca1d13563d88f75c681a718a6ca8ebd087
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-04-12 22:44:56 +0300

    Update dashboard store

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -1,18 +1,45 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import userReducer from './slices/user'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import authReducer from './slices/auth'
 import productsReducer from './slices/products'
 import ordersReducer from './slices/orders'
+import usersReducer from './slices/users'
+
+const persistConfig = {
+  key: 'auth',
+  version: 1,
+  storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 
 const rootReducer = combineReducers({
-  user: userReducer,
+  auth: persistedAuthReducer,
   products: productsReducer,
-  orders: ordersReducer
+  orders: ordersReducer,
+  users: usersReducer
 })
 
 const store = configureStore({
-  reducer: {
-    reducer: rootReducer
-  }
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 })
+
+export const persistor = persistStore(store)
 
 export default store
